8e74649d8dab3d4132a8a1e8882c5bccda7edb23
Enrique Soriano <jenriquesoriano@guadaltel.com>
2022-03-16 18:12:53 +0100

    dockerui

---
+++ js/config.js
@@ -0,0 +1,23 @@
+var captchaEnabled = false;
+var betaBanner = false;
+var labelStaging = false;
+var environment = "PROD";
+// PROD
+//var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+//var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
+//var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+// SANDBOX
+// var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+// var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+// var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+// STAGING
+//var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+//var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+//var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
+// PROD CONTRACTOR
+var serverURL = "http://localhost:8090/validator/v2/";
+var serverRealURL = "http://localhost:8090/validator/v2/";
+var serverDirectURL = "http://localhost:8090/validator/v2/";
+var serverCaptchaURL = "http://localhost:8090/validator/v2/validator/captcha/verify.php";
+var serverToken = "BQIAJulY249NyeiT6gsfc9AUTCa8Nkgi72J9jIVP";
+
